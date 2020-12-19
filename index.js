d996d7adff913ea44b1de7fa2f28293292df52e7
ykl1 <lyoungkwang.yl@gmail.com>
2020-12-18 22:59:00 -0500

    initial change

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import App from './src/components/App'
 
-ReactDOM.render(<App/>, document.getElementById('react-root'))
+const store = createStore(rootReducer)
+
+render (
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  document.getElementById('react-root')
+)
